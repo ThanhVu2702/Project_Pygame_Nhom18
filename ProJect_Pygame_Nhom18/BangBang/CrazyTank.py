640f882b5826ca3fa07685560b5e898ea1387e74
𝙏𝙝𝙖𝙣𝙝 𝙑𝙪 𓆣 <thanhvu270202@gmail.com>
2024-03-10 09:48:58 +0700

    Project
    bat dau du an thoi nao !!!

---
+++ ProJect_Pygame_Nhom18/BangBang/CrazyTank.py
@@ -0,0 +1,95 @@
+import pgzrun, random
+from random import randint
+
+WIDTH, HEIGHT = 800, 600
+WHITE, VIOLET, BLUE = (255,255,255), (255, 182, 193), (0, 128, 255)
+
+# Khai báo biến
+diem = 0
+ket_thuc = False
+
+# Định dạng hình nền
+background = Actor("nengame")
+
+# Định dạng tank
+tank = Actor('captain')
+tank.x, tank.y = 100, 550
+
+# Định dạng hộp quà
+item = Actor('hopqua')
+item.x, item.y = 200, 0
+
+# Định dạng viên thiên thạch
+rock = Actor('thienthach')
+rock.x, rock.y = 400, 0
+
+# Set up điểu khiển tank bằng chuột
+def on_mouse_move(pos,rel,buttons):
+    tank.x = pos[0]
+    tank.y = pos[1]
+
+sounds.nhacnen.play()
+sounds.nhacnen.set_volume(0.5)
+
+# Reset game
+def reset_game():
+    global diem, ket_thuc
+    diem = 0
+    ket_thuc = False
+    tank.x, tank.y = 100, 550
+    item.x, item.y = 200, 0
+    rock.x, rock.y = 400, 0
+
+
+# set up các vật thể & tính điểm, cộng điểm khi nhặt được hộp quà
+def update():
+    global diem, ket_thuc
+    
+    # Tăng tốc độ rơi của thiên thạch dựa vào điểm mà player đang có
+    item.y = item.y + 2 + diem / 4
+    rock.y = rock.y + 2 + diem / 4
+
+    # Set up giới hạn của màn hình
+    if item.y > 600:
+        item.y = 0
+    
+    # Thiên thạch này rơi xong thì random lại thiên thạch rơi tiếp
+    if rock.y > 600:
+        rock.y = 0
+        rock.x = random.randint(20,780)
+        
+
+    # item và thiên thạch không được xuất hiện ngẫu nhiên cùng một chỗ
+    while rock.colliderect(item):
+        rock.x = random.randint(20,780)
+        item.x = random.randint(20,780)
+
+    # Xe tăng nhặt hộp quà
+    if item.colliderect(tank):
+        item.x = random.randint(20,780)
+        item.y = 0
+        diem += 1
+        
+    # Thiên thạch rơi trúng xe tăng
+    if rock.colliderect(tank):
+        ket_thuc = True
+        sounds.bum.play()
+        sounds.nhacnen.stop()
+
+# Đưa các đối tượng và thông báo ra màn hình
+def draw():
+    background.draw()
+    if ket_thuc:
+        screen.draw.text('You Lose',(210,200), color = WHITE, fontsize=60)
+        screen.draw.text('Total Score: '+ str(diem),(210,250), color = VIOLET, fontsize=80)
+        screen.draw.text('(Press G to play again)',(210,330), color = BLUE, fontsize=40)
+        if keyboard.G:
+            reset_game()
+            sounds.nhacnen.play()
+            sounds.nhacnen.set_volume(0.5)
+    else:
+      screen.draw.text('Score: '+ str(diem),(10,15), color = WHITE, fontsize=60)
+      tank.draw()
+      item.draw()
+      rock.draw()
+pgzrun.go()
---
+++ ProJect_Pygame_Nhom18/BangBang/main.py
@@ -0,0 +1,73 @@
+import sys
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
+from PyQt5.QtCore import pyqtSlot
+import subprocess
+#import thư viện pygame để chạy nhạc nền
+import pygame 
+
+class App(QWidget):
+#khởi tạo cửa sổ ứng dụng với tiêu đề, kích thước và vị trí cụ thể
+    def __init__(self):
+        super().__init__()
+        self.title = 'PROJECT PYGAME NHÓM 18'
+        self.left = 700
+        self.top = 300
+        self.width = 728
+        self.height = 410
+        self.initUI()
+   
+  
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        # Phát nhạc khi mở game
+        pygame.init()
+        pygame.mixer.init()
+        sound = pygame.mixer.Sound('BangBang/sounds/musicbg.wav')
+        sound.play()
+
+        # Tạo label hiển thị hình ảnh
+        label = QLabel(self)
+        pixmap = QPixmap('BangBang/images/background.png')
+        label.setPixmap(pixmap)
+        label.setGeometry(0, 0, self.width, self.height)
+
+        # Tạo 2 button, mỗi button là 1 game
+    
+        btn1 = QPushButton('Tank PvP', self)
+        btn1.move(90, 370) #đặt nút btn1 ở vị trí có tọa độ x: 230 và y: 350 trên cửa sổ giao diện
+        btn1.clicked.connect(self.on_click_mode1)
+
+        btn2 = QPushButton('Crazy Tank', self)
+        btn2.move(300, 370) #đặt nút btn2 ở vị trí có tọa độ x: 380 và y: 350 trên cửa sổ giao diện
+        btn2.clicked.connect(self.on_click_mode2)
+
+        # Tạo button thoát
+        btn_exit = QPushButton('Thoát', self)
+        btn_exit.move(510, 370) #đặt nút exit ở vị trí có tọa độ x: 230 và y: 350 trên cửa sổ giao diện
+        btn_exit.clicked.connect(self.on_click_exit)
+
+        self.show()
+
+    #click vào button Tank PvP để chạy TankPvP.py
+    @pyqtSlot()
+    def on_click_mode1(self):
+        subprocess.run(['python', 'BangBang/tank-game-master/TankPvP.py'])
+ 
+    #click vào button Crazy Tank để chạy game CrazyTank.py
+    @pyqtSlot()
+    def on_click_mode2(self):
+        subprocess.run(['python', 'BangBang/CrazyTank.py'])
+
+    @pyqtSlot()
+    def on_click_exit(self):
+        # Thoát chương trình
+        QApplication.quit()
+    
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
